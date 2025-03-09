60efc44133ac7dc8146a212a6d32af54a98cb965
mariosantos-05 <marioaugustov.s@gmail.com>
2025-03-09 15:49:17 -0300

    Add of light equations

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -1,12 +1,33 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
 in vec2 TexCoord;
+in vec3 FragPos;
+in vec3 Normal;
 
 uniform sampler2D texture1;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
 
-void main()
-{
-    FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
+void main() {
+    // Ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // Specular
+    float specularStrength = 0.8;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (diffuse+ ambient +specular) * texture(texture1, TexCoord).rgb;
+    FragColor = vec4(result, 1.0);
 }
