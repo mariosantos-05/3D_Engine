845b41a2120e84b55f1de7a126f5766d7b2b649b
mariosantos-05 <marioaugustov.s@gmail.com>
2025-03-09 16:06:32 -0300

    Specular Strength mod

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -22,7 +22,7 @@ void main() {
     vec3 diffuse = diff * lightColor;
 
     // Specular
-    float specularStrength = 0.8;
+    float specularStrength = 1.0;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
